ef1def68c984582400d8938cbcfde57c51e2a834
afternone <afternone@163.com>
2015-09-23 10:52:27 +0800

    first version of GraphPlot

--- src/GraphPlot.jl
+++ src/GraphPlot.jl
@@ -1,5 +1,17 @@
 module GraphPlot
+    if VERSION < v"0.4.0"
+        using Docile
+    end
+    using Graphs
+    using Compose  # for plotting features
+    import Graphs.plot
 
-# package code goes here
+    # layout algorithms
+    export random_layout, circular_layout
+    include("layout.jl")
+
+    # ploting utilities
+    export plot
+    include("plot.jl")
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,51 @@
+using FactCheck
 using GraphPlot
-using Base.Test
+using Graphs
+using Colors
 
-# write your own tests here
-@test 1 == 1
+cachedout = joinpath(Pkg.dir("GraphLayout"), "test", "examples")
+facts("random_layout") do
+    context("simple_house_graph") do
+        g = simple_house_graph()
+        filename = joinpath(cachedout, "simple_house_random.svg")
+        GraphPlot.plot(g, filename=filename)
+    end
+    context("simple_wheel_graph") do
+        g = simple_wheel_graph(10)
+        filename = joinpath(cachedout, "simple_wheel_random.svg")
+        GraphPlot.plot(g, filename=filename)
+    end
+end
+
+facts("circular_layout") do
+    context("simple_house_graph") do
+        g = simple_house_graph()
+        filename = joinpath(cachedout, "simple_house_circular.svg")
+        GraphPlot.plot(g, filename=filename, layout=circular_layout)
+    end
+    context("simple_wheel_graph") do
+        g = simple_wheel_graph(10)
+        filename = joinpath(cachedout, "simple_wheel_circular.svg")
+        GraphPlot.plot(g, filename=filename, layout=circular_layout)
+    end
+end
+
+facts("Nodes have different colors and their size are proportional to their degree") do
+    context("simple_house_graph") do
+        g = simple_house_graph()
+        nodesize = Float64[out_degree(v, g) for v in vertices(g)]
+        # Generate n maximally distinguishable colors in LCHab space.
+        nodefillc = distinguishable_colors(num_vertices(g), colorant"red")
+        filename = joinpath(cachedout, "simple_house_graph.svg")
+        GraphPlot.plot(g, filename=filename, layout=circular_layout,
+             nodefillc=nodefillc, nodesize=nodesize)
+    end
+    context("simple_wheel_graph") do
+        g = simple_wheel_graph(10)
+        nodesize = Float64[out_degree(v, g) for v in vertices(g)]
+        nodefillc = distinguishable_colors(num_vertices(g), colorant"red")
+        filename = joinpath(cachedout, "simple_wheel_graph.svg")
+        GraphPlot.plot(g, filename=filename, layout=circular_layout,
+             nodefillc=nodefillc, nodesize=nodesize)
+    end
+end
