ee0f192d75a283c9a40d6fc340333e8e49fb7015
afternone <afternone@163.com>
2015-10-18 14:55:00 +0800

    simple LightGraphs integration

--- src/GraphPlot.jl
+++ src/GraphPlot.jl
@@ -5,8 +5,8 @@ if VERSION < v"0.4.0"
 end
 
 using Compat
-using Graphs
-using LightGraphs
+import Graphs
+import LightGraphs
 using Compose  # for plotting features
 
 const gadflyjs = joinpath(dirname(Base.source_path()), "gadfly.js")
@@ -38,22 +38,22 @@ function test()
 	include(joinpath(Pkg.dir("GraphPlot"), "test", "runtests.jl"))
 end
 
-_nv{V}(g::AbstractGraph{V}) = Graphs.num_vertices(g)
+_nv{V}(g::Graphs.AbstractGraph{V}) = Graphs.num_vertices(g)
 _nv(g::LightGraphs.SimpleGraph) = LightGraphs.nv(g)
-_ne{V}(g::AbstractGraph{V}) = Graphs.num_edges(g)
+_ne{V}(g::Graphs.AbstractGraph{V}) = Graphs.num_edges(g)
 _ne(g::LightGraphs.SimpleGraph) = LightGraphs.ne(g)
-_vertices{V}(g::AbstractGraph{V}) = Graphs.vertices(g)
+_vertices{V}(g::Graphs.AbstractGraph{V}) = Graphs.vertices(g)
 _vertices(g::LightGraphs.SimpleGraph) = LightGraphs.vertices(g)
-_edges{V}(g::AbstractGraph{V}) = Graphs.edges(g)
+_edges{V}(g::Graphs.AbstractGraph{V}) = Graphs.edges(g)
 _edges(g::LightGraphs.SimpleGraph) = LightGraphs.edges(g)
-_src_index{V}(e::Graphs.Edge{V}, g::AbstractGraph{V}) = Graphs.vertex_index(Graphs.source(e), g)
+_src_index{V}(e::Graphs.Edge{V}, g::Graphs.AbstractGraph{V}) = Graphs.vertex_index(Graphs.source(e), g)
 _src_index(e::LightGraphs.Edge, g::LightGraphs.SimpleGraph) = LightGraphs.src(e)
-_dst_index{V}(e::Graphs.Edge{V}, g::AbstractGraph{V}) = Graphs.vertex_index(Graphs.target(e), g)
+_dst_index{V}(e::Graphs.Edge{V}, g::Graphs.AbstractGraph{V}) = Graphs.vertex_index(Graphs.target(e), g)
 _dst_index(e::LightGraphs.Edge, g::LightGraphs.SimpleGraph) = LightGraphs.dst(e)
-_adjacency_matrix{V}(g::AbstractGraph{V}) = Graphs.adjacency_matrix(g)
+_adjacency_matrix{V}(g::Graphs.AbstractGraph{V}) = Graphs.adjacency_matrix(g)
 _adjacency_matrix(g::LightGraphs.SimpleGraph) = LightGraphs.adjacency_matrix(g)
-_is_directed{V}(g::AbstractGraph{V}) = Graphs.is_directed(g)
-_is_directed(g::Union{LightGraphs.DiGraph, LightGraphs.Graph}) = LightGraphs.is_directed(g)
-_laplacian_matrix{V}(g::AbstractGraph{V}) = Graphs.laplacian_matrix(g)
+_is_directed{V}(g::Graphs.AbstractGraph{V}) = Graphs.is_directed(g)
+_is_directed(g::LightGraphs.SimpleGraph) = LightGraphs.is_directed(g)
+_laplacian_matrix{V}(g::Graphs.AbstractGraph{V}) = Graphs.laplacian_matrix(g)
 _laplacian_matrix(g::LightGraphs.SimpleGraph) = LightGraphs.laplacian_matrix(g)
 end # module
