04e05a6470eff0703006310c1b2bd76f8926d585
Júlio Hoffimann <julio.hoffimann@gmail.com>
2017-06-25 11:46:22 -0700

    Use Base.Test instead of FactCheck (#42)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,8 @@
-using FactCheck
 using GraphPlot
 using LightGraphs
 using Colors
 using Compose
+using Base.Test
 
 g = graphfamous("karate")
 h = LightGraphs.WheelGraph(10)
@@ -11,29 +11,28 @@ nodelabel = collect(1:nv(g))
 nodesize = outdegree(g) .* 1.0
 
 cachedout = joinpath(Pkg.dir("GraphPlot"), "test", "examples")
-facts("karate network (undirected), nodesize is proportion to its degree") do
+
+@testset "Undirected Karate Net" begin
     filename = joinpath(cachedout, "karate_different_nodesize.svg")
     draw(SVG(filename, 8inch, 8inch), gplot(g, nodesize=nodesize.^0.3, nodelabel=nodelabel, nodelabelsize=nodesize.^0.3))
 end
 
-facts("karate network as directed") do
-    FactCheck.context("straight line edge") do
-        filename = joinpath(cachedout, "karate_straight_directed.svg")
-        draw(SVG(filename, 10inch, 10inch), gplot(g, arrowlengthfrac=0.02, nodelabel=nodelabel))
-    end
+@testset "Directed Karate Net" begin
+    filename = joinpath(cachedout, "karate_straight_directed.svg")
+    draw(SVG(filename, 10inch, 10inch), gplot(g, arrowlengthfrac=0.02, nodelabel=nodelabel))
 end
 
-facts("Nodes in different memberships have different colors (karate network)") do
+@testset "Membership" begin
     # nodes membership
-	membership = [1,1,1,1,1,1,1,1,2,1,1,1,1,1,2,2,1,1,2,1,2,1,2,2,2,2,2,2,2,2,2,2,2,2]
-	nodecolor = [colorant"lightseagreen", colorant"orange"]
-	# membership color
-	nodefillc = nodecolor[membership]
+    membership = [1,1,1,1,1,1,1,1,2,1,1,1,1,1,2,2,1,1,2,1,2,1,2,2,2,2,2,2,2,2,2,2,2,2]
+    nodecolor = [colorant"lightseagreen", colorant"orange"]
+    # membership color
+    nodefillc = nodecolor[membership]
     filename = joinpath(cachedout, "karate_groups.svg")
     draw(SVG(filename, 8inch, 8inch), gplot(g, nodelabel=nodelabel, nodefillc=nodefillc))
 end
 
-facts("LightGraphs test") do
+@testset "WheelGraph" begin
     filename = joinpath(cachedout, "LightGraphs_wheel10.svg")
     draw(SVG(filename, 8inch, 8inch), gplot(h))
 end
