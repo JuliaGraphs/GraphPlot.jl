20e16b1668b034a9720669d8628c84a4dc1681df
Júlio Hoffimann <julio.hoffimann@gmail.com>
2017-07-08 18:18:44 -0700

    Add visual regression tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,36 +3,49 @@ using LightGraphs
 using Colors
 using Compose
 using Base.Test
+using VisualRegressionTests
 
+# global variables
+istravis = "TRAVIS" ∈ keys(ENV)
+datadir = joinpath(@__DIR__, "data")
+
+# graphs to test
 g = graphfamous("karate")
 h = LightGraphs.WheelGraph(10)
 
-nodelabel = collect(1:nv(g))
-nodesize = outdegree(g) .* 1.0
-
-cachedout = joinpath(@__DIR__, "examples")
-
-@testset "Undirected Karate Net" begin
-    filename = joinpath(cachedout, "karate_different_nodesize.svg")
-    draw(SVG(filename, 8inch, 8inch), gplot(g, nodesize=nodesize.^0.3, nodelabel=nodelabel, nodelabelsize=nodesize.^0.3))
+# plot and save function for visual regression tests
+function plot_and_save(fname, g; gplot_kwargs...)
+    srand(2017)
+    draw(PNG(fname, 8inch, 8inch), gplot(g; gplot_kwargs...))
 end
 
-@testset "Directed Karate Net" begin
-    filename = joinpath(cachedout, "karate_straight_directed.svg")
-    draw(SVG(filename, 10inch, 10inch), gplot(g, arrowlengthfrac=0.02, nodelabel=nodelabel))
-end
+@testset "Karate Net" begin
+    # auxiliary variables
+    nodelabel = collect(1:nv(g))
+    nodesize = outdegree(g) .* 1.0
 
-@testset "Membership" begin
-    # nodes membership
+    # test nodesize
+    plot_and_save1(fname) = plot_and_save(fname, g, nodesize=nodesize.^0.3, nodelabel=nodelabel, nodelabelsize=nodesize.^0.3)
+    refimg1 = joinpath(datadir, "karate_different_nodesize.png")
+    @test test_images(VisualTest(plot_and_save1, refimg1), popup=!istravis) |> success
+
+    # test directed graph
+    plot_and_save2(fname) = plot_and_save(fname, g, arrowlengthfrac=0.02, nodelabel=nodelabel)
+    refimg2 = joinpath(datadir, "karate_straight_directed.png")
+    @test test_images(VisualTest(plot_and_save2, refimg2), popup=!istravis) |> success
+
+    # test node membership
     membership = [1,1,1,1,1,1,1,1,2,1,1,1,1,1,2,2,1,1,2,1,2,1,2,2,2,2,2,2,2,2,2,2,2,2]
     nodecolor = [colorant"lightseagreen", colorant"orange"]
-    # membership color
     nodefillc = nodecolor[membership]
-    filename = joinpath(cachedout, "karate_groups.svg")
-    draw(SVG(filename, 8inch, 8inch), gplot(g, nodelabel=nodelabel, nodefillc=nodefillc))
+    plot_and_save3(fname) = plot_and_save(fname, g, nodelabel=nodelabel, nodefillc=nodefillc)
+    refimg3 = joinpath(datadir, "karate_groups.png")
+    @test test_images(VisualTest(plot_and_save3, refimg3), popup=!istravis) |> success
 end
 
 @testset "WheelGraph" begin
-    filename = joinpath(cachedout, "LightGraphs_wheel10.svg")
-    draw(SVG(filename, 8inch, 8inch), gplot(h))
+    # default options
+    plot_and_save1(fname) = plot_and_save(fname, h)
+    refimg1 = joinpath(datadir, "wheel10.png")
+    @test test_images(VisualTest(plot_and_save1, refimg1), popup=!istravis) |> success
 end
